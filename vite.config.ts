62f0d39eb623231758f192c76ac5c3e25e9c5699
Akash <akash7501@gmail.com>
2026-04-06 00:43:46 +0530

    addes bases path

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { federation } from "@module-federation/vite";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import { federation } from "@module-federation/vite"
 
 export default defineConfig({
+  base: "https://mfe-container-repo.s3.ap-south-1.amazonaws.com/products/",
+
   plugins: [
     react(),
     federation({
       name: "products",
       filename: "remoteEntry.js",
+
       exposes: {
         "./ProductsPage": "./src/ProductsPage.jsx"
       },
+
       shared: ["react", "react-dom"]
     })
-  ],
-  server: {
-    cors: true,
-    port: 3001,
-     headers: {
-    "Access-Control-Allow-Origin": "*"
-  }
-  }
-});
+  ]
+})
